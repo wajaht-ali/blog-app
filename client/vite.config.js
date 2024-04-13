7b7604e068b3547af2bf2804eb2b0c61c0b362e8
Wajahat Ali <contact.chwajahat@gmail.com>
2024-04-13 11:31:31 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   proxy: {
-      "/api": "http://localhost:8000",
+      "/api": "https://the-news-app-ten.vercel.app",
     },
 })
